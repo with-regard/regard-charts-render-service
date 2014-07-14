cd67099991d454008c37580c4dd961b80ed8c15a
Kevin Boyle <kevin.boyle@outlook.com>
2014-07-14 23:53:48 +0100

    call the rendering during the post

---
+++ sample.js
@@ -0,0 +1,16 @@
+module.exports = {
+        chart: {
+            width: 300,
+            height: 300,
+            defaultSeriesType: 'bar'
+        },
+        legend: {
+            enabled: false
+        },
+        title: {
+            text: 'Highcharts rendered by Node!'
+        },
+        series: [{
+            data: [ 1, 2, 3, 4, 5, 6 ]
+        }]
+    };
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var highcharts = require('node-highcharts');
+var sampleOptions = require('./sample.js');
 
 var app = express();
 
 app.use(bodyParser.json());
-app.post('/render', function(req, res, next) { 
-  console.log(req.body);
-  res.json({
-    url: 'some url'
-  });
+app.post('/render', function(req, res, next) {
+  highcharts.render(sampleOptions, function(err, data) {
+    if (err) {
+      console.log('Error: ' + err);
+      next();
+    } else {
+      res.json({
+        url: 'some url',
+        data: data
+      });
+    }
+  }); 
 });
 
 app.listen(3000);
