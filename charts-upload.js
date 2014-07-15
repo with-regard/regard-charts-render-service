626ab63fc66fa782079f44f21361950731b844ea
Kevin Boyle <kevin.boyle@outlook.com>
2014-07-15 11:37:45 +0100

    allow uploading to azure

---
+++ charts-upload.js
@@ -0,0 +1,18 @@
+var azure = require('azure-storage');
+
+var blobSvc = azure.createBlobService();
+
+module.exports = {
+  upload: function(data) {
+    return new Promise(function(fulfil, reject) {
+          var stream = blobSvc.createWriteStreamToBlockBlob('regardcharts', 'somefile2');
+
+          stream.on('error', reject);
+          stream.on('close', function() {
+	    fulfil('some url');
+	  });
+
+          stream.end(data);
+    });
+  }
+}
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var highcharts = require('node-highcharts');
+var azure = require('azure-storage');
+var Promise = require('promise');
+var charts = require('./charts-upload.js');
 var sampleOptions = require('./sample.js');
 
 var app = express();
@@ -9,15 +12,17 @@ app.use(bodyParser.json());
 app.post('/render', function(req, res, next) {
   highcharts.render(sampleOptions, function(err, data) {
     if (err) {
-      console.log('Error: ' + err);
-      next();
+      next(err);
     } else {
-      res.json({
-        url: 'some url',
-        data: data
+      charts.upload(data).then(function(url) {
+        res.json({
+          url: 'some url'
+        });
+      }, function (err) {
+        next(err);
       });
     }
   }); 
 });
 
-app.listen(80);
+app.listen(8080);
